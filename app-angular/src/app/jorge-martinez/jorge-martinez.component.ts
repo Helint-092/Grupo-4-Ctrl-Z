9bde1d3ce3413fff5c9adf5c4f198a27f7aaa05b
Jorge Martinez <jm703923@gmail.com>
2025-10-30 11:40:44 -0600

    Se Agrego el Componente

---
+++ app-angular/src/app/jorge-martinez/jorge-martinez.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jorge-martinez',
+  imports: [],
+  templateUrl: './jorge-martinez.component.html',
+  styleUrl: './jorge-martinez.component.scss'
+})
+export class JorgeMartinezComponent {
+
+}
